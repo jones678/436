f9802fe30941d402bf4fb70ee6555f771a6f524a
Joseph Jones <jonesjay@suddenlink.net>
2014-03-24 13:03:37 -0400

    Added more and hide to the pricing page.

--- js/main.js
+++ js/main.js
@@ -1,9 +1,10 @@
-function onDeviceReady(){ 
-	$(document).ready(function(){
-              alert("i\'m working");        
-                                       });
-                      
-}
-/** Called when browser load this page*/ 
-function init(){
-document.addEventListener("deviceready", onDeviceReady, false); }
+$( document).bind( 'pageinit',function(event){
+  $('#expand').bind("tap", function(e){
+  	$('#expand').css("display","none");
+  	$("#priceInfo").css("display","block");
+  	});//end tap
+  $('#hide').bind("tap",function(e){
+  	$('#priceInfo').css("display","none");
+  	$('#expand').css("display","block");
+  });
+});// end pageinit
